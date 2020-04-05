bc380fec49a790fc443968a023422c2580c5c3b0
Erik <erikgregorywebb@gmail.com>
2020-04-05 13:12:33 -0400

    Update analysis.R

--- code/analysis.R
+++ code/analysis.R
@@ -4,10 +4,28 @@ library(tidyverse)
 library(tidytext)
 library(visNetwork)
 
-# import
+# import package details 
 url = 'https://raw.githubusercontent.com/erikgregorywebb/CRAN-packages/master/data/cran-packages-detail.csv'
 cran = read_csv(url)
 glimpse(cran)
+
+# import package downloads 
+url = 'https://raw.githubusercontent.com/erikgregorywebb/CRAN-packages/master/data/cran-packages-downloads.csv'
+downloads = read_csv(url)
+glimpse(downloads)
+
+# aggregate monthly downloads, merge
+cran = left_join(
+  x = cran,
+  y = downloads %>%
+    group_by(package) %>% summarise(last_month_downloads = sum(count)),
+  by = c('name' = 'package')
+)
+
+# define cutoff
+top_packages = cran %>%
+  filter(last_month_downloads >= quantile(cran$last_month_downloads, .995)) %>%
+  distinct(name) %>% pull(name)
 
 # define edges
 edges = cran %>%
@@ -16,10 +34,13 @@ edges = cran %>%
   unnest_tokens(output = word, input = value, token = 'words') %>%
   filter(word != 'r') %>%
   filter(!str_detect(word, '^[0-9]')) %>%
-  rename(from = name, to = word)
+  rename(from = name, to = word) %>%
+  filter(from %in% top_packages | to %in% top_packages)
 
 # define nodes
-nodes = bind_rows(edges %>% select(package = from), edges %>% select(package = to)) %>% distinct(package)
+nodes = bind_rows(edges %>% select(id = from), 
+                  edges %>% select(id = to)) %>% distinct(id) %>%
+  mutate(label = id)
 
 # build visual
 visNetwork(nodes = nodes, edges = edges)
